efb58d0b5505ba8ab67cc996b1a0902acbffdfda
cvan90 <cvanv90@yahoo.com>
2025-04-07 18:16:24 -0400

    linked pages together

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -3,6 +3,7 @@ import {HashRouter, Routes, Route} from "react-router-dom"
 import LogIn from "./components/LogIn"
 import Register from "./components/Register"
 import NavBar from "./components/NavBar";
+import Dashboard from "./components/Dashboard";
 import "./App.css"
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/LogIn" element={<LogIn/>}/>
         <Route path="/Register" element={<Register/>}/>
+        <Route path="/Dashboard" element={<Dashboard/>}/>
       </Routes>
     </HashRouter>   
   
