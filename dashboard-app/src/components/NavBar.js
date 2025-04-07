416ca3fa5e8ee1a59baf80ed22ea6a78a2e8df5d
cvan90 <cvanv90@yahoo.com>
2025-04-07 18:44:36 -0400

    added css to nav bar

--- dashboard-app/src/components/NavBar.js
+++ dashboard-app/src/components/NavBar.js
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 function NavBar(){
     return (
         <>
-        <nav>
-            <Link to="/">Home</Link>{" | "}
+        <nav className="navbar" >
+            <Link to="/">Home</Link>
           
         </nav>
         </>
