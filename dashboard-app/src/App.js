845aad06cb4912755018f3d83255f5bd849708ca
cvan90 <cvanv90@yahoo.com>
2025-04-07 14:53:26 -0400

    testing a fix to react router

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,15 +1,15 @@
+import {HashRouter,Routes,Route} from "react-router-dom";
 import Home from "./components/Home";
-import {HashRouter,Routes,Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <>
-    <HashRouter>
+   <HashRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-    </Routes>
-  </HashRouter>
+      </Routes>
+      </HashRouter>
     </>
   );
 }
