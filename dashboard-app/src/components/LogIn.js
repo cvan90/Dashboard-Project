491d9bcc2d6260515bda0e4a4f24351cc9200994
cvan90 <cvanv90@yahoo.com>
2025-04-11 16:08:05 -0400

    fixed errors in login code

--- dashboard-app/src/components/LogIn.js
+++ dashboard-app/src/components/LogIn.js
@@ -1,7 +1,8 @@
-import {Link} from "react-router-dom";
-import {useRef, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import {useRef, useState, useEffect} from "react";
 function LogIn()
  {
+  let navigate = useNavigate();
   const usernamesubmit=useRef("")
   const userpasssubmit=useRef("")
   const [msgText, setMsg]=useState("")
@@ -13,6 +14,48 @@ function LogIn()
   console.log("Login Submitted")
   console.log(`username:${usernamesubmit.current.value} password: ${userpasssubmit.current.value}`)
 
+  let _msg ="";
+  let _replit_url= "https://9246a3e9-3cb0-4bc9-a109-e01a5eec5873-00-dq79sb088n9e.worf.replit.dev";
+
+  const _uid =usernamesubmit.current.value
+  const _pwd = userpasssubmit.current.value
+
+  const _url = `https://${_replit_url}/login/${_uid}/${_pwd}`; 
+
+  if(_uid === null || _uid.trim().length === 0)
+    {
+        _msg = "Invalid username Submitted";                
+        setMsg(_msg)                
+        return false;
+    }
+    
+    if(_pwd === null || _pwd.trim().length === 0)
+    {
+        _msg = "invalid password Submitted";                
+        setMsg(_msg)                
+        return false;
+    }
+    
+    fetch(_url)
+    .then((res)=>res.json())
+    .then((data)=> {
+        
+        if(data.login === true)
+        {
+            console.log("navigate to dashboard")
+            navigate("/dashboard")
+            //navigate("/dashboard",{replace:true})
+        }
+
+        setMsg(data.msg)
+    })
+    .catch((error)=>{
+        setMsg("request error");
+        console.log("request error *");
+        console.log(error);
+    });
+    usernamesubmit.current.value="";
+    userpasssubmit.current.value="";
 }
     return (
         <>
