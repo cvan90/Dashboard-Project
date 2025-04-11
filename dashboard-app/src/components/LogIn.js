336205655edf536c78ac642ac55834d1472c14eb
cvan90 <cvanv90@yahoo.com>
2025-04-11 15:31:03 -0400

    coded in login parameters to start implementing login

--- dashboard-app/src/components/LogIn.js
+++ dashboard-app/src/components/LogIn.js
@@ -1,16 +1,29 @@
 import {Link} from "react-router-dom";
-function LogIn() {
+import {useRef} from "react";
+function LogIn()
+ {
+  const usernamesubmit=useRef("")
+  const userpasssubmit=useRef("")
 
+  const loginsubmit=(e)=>{
+
+  e.preventDefault()
+
+  console.log("Login Submitted")
+
+}
     return (
         <>
         <div>
         <h2>Login</h2>
       </div>
       <form>
-        <label>UserName:</label><input type="text" placeholder="Enter Username" maxLength={25}></input><br/>
-        <label>Password:</label><input type="text" placeholder="Enter PassWord" maxLength={25}></input>
+        <label>UserName:</label><input ref={usernamesubmit} type="text" placeholder="Enter Username" maxLength={25}></input><br/>
+        <label>Password:</label><input ref={userpasssubmit} type="text" placeholder="Enter PassWord" maxLength={25}></input>
         <p></p>
-        <button>Submit</button>
+        <p>{msgText}</p>
+
+        <button onClick={(e)=>loginsubmit(e)}>Submit</button>
       </form>
       <p></p>
        <p><Link to="/Register">Register</Link></p>
